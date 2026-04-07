e8796a22f0458dbdaa284c8acb0ebee6edf79096
Shubhanjil Dhuria <shubhanjild@gmail.com>
2026-04-07 10:16:25 +0530

    Refactor delegate examples and update method calls

--- delegate.cs
+++ delegate.cs
@@ -3,7 +3,11 @@ using System;
 public class DelegateExamples{
     
     // BASIC DELEGATE
+    // return type is set to void and method must take single string input
     public delegate void MyDelegate(string message);
+    
+    // return type is set to void and method must take 2 inputs (1 string and 1 int)
+    public delegate void MyDelegateTwo(string item, int price);
 
     public static void PrintMessage(string msg){
         Console.WriteLine("Message: " + msg);
@@ -15,33 +19,35 @@ public class DelegateExamples{
     }
 
     // DELEGATE WITH RETURN VALUE
+    // return type is set to int and method must take 2 integers
     public delegate int MathOperation(int a, int b);
     public static int Add(int x, int y) => x + y;
     public static int Multiply(int x, int y) => x * y;
 
     public static void Main(){
         Console.WriteLine("BASIC DELEGATE");
+        // delegate_name var = method_name
         MyDelegate del1 = PrintMessage;
-        del1("Hello, Delegates!\n");
+        del1("Hello, Delegates!");
 
-        Console.WriteLine("MULTICAST DELEGATE");
+        Console.WriteLine("\nMULTICAST DELEGATE");
         MyDelegate del2 = PrintMessage;
         del2 += ShowMessage;
-        del2("Delegates are powerful!\n");
+        del2("Delegates are powerful!");
 
-        Console.WriteLine("DELEGATE WITH RETURN VALUE");
+        Console.WriteLine("\nDELEGATE WITH RETURN VALUE");
         MathOperation op1 = Add;
         MathOperation op2 = Multiply;
         Console.WriteLine("Add: " + op1(5, 3));
-        Console.WriteLine("Multiply: " + op2(5, 3) +"\n");
+        Console.WriteLine("Multiply: " + op2(5, 3));
 
-        Console.WriteLine("ANONYMOUS METHOD");
-        MyDelegate del3 = delegate(string msg){
-            Console.WriteLine("Anonymous says: " + msg);
+        Console.WriteLine("\nANONYMOUS METHOD");
+        MyDelegateTwo del3 = delegate(string item, int price){
+            Console.WriteLine("Anonymous says: Item Name="+item+ "\tItem Price="+price);
         };
-        del3("Hello from anonymous method!\n");
+        del3("Pen",35);
 
-        Console.WriteLine("LAMBDA EXPRESSION");
+        Console.WriteLine("\nLAMBDA EXPRESSION");
         MathOperation add = (x, y) => x + y;
         MathOperation multiply = (x, y) => x * y;
         Console.WriteLine("Add: " + add(10, 20));
